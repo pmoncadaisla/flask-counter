e21009a55327c89c4753c81ce5c727dd1756a97d
Pablo Moncada <pablo.moncada@masmovil.com>
2019-09-19 23:40:24 +0200

    Version 1.2.0

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t eu.gcr.io/pmoncada-001/keepcoding/flask-counter:v1.1.0 .
-docker push eu.gcr.io/pmoncada-001/keepcoding/flask-counter:v1.1.0
+docker build -t eu.gcr.io/pmoncada-001/keepcoding/flask-counter:v1.2.0 .
+docker push eu.gcr.io/pmoncada-001/keepcoding/flask-counter:v1.2.0
